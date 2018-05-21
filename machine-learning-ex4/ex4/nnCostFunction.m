5eb01cd1b2953924a0cb15b7563988ba551802d0
Akram Alraai <akram.alraai@gmail.com>
2018-05-21 17:06:08 +0200

    backpropagation algo

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -38,6 +38,47 @@ Theta2_grad = zeros(size(Theta2));
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
+
+
+% Feedforward propagation
+
+a1 = [ones(m, 1) X];
+
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2,1), 1) a2];
+
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+hThetaX = a3; %size(hThetaX) = 10 x 5000
+
+%  see https://www.coursera.org/learn/machine-learning/resources/Uuxg6
+% this approach is used for the double summation for the cost function
+% A = 2x2 , B = 2x2, then sum(sum(A .* B)) = a11*b11 + a12*b12 + a21*b21 + a22*b22
+%
+
+
+%size(yVec) = 5000 x 10 where entries (1,10) .. (500,10), (501,1), .. (1000, 1), (1001,2), ... (5000, 9) are equal to one the other
+% entries are zero
+yVec = zeros(m,num_labels);
+
+for i = 1:m
+    yVec(i,y(i)) = 1;
+end
+
+% cost function without regularization
+
+J = 1/m * sum(sum(-1 * yVec .* log(hThetaX)-(1-yVec) .* log(1-hThetaX)));
+
+
+% Note that you should not be regularizing the terms that correspond to the bias.
+% For the matrices Theta1 and Theta2, this corresponds to the first column of each matrix.
+
+Regular =  lambda/(2*m) *  (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)));
+
+J = J + Regular;
+
+
 %
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
@@ -46,14 +87,50 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
+
+
+for t=1:m
+
+    % 1.
+    %Give me the t-th row training example and add bias unit
+    a1 = [1; X(t,:)'];
+
+    z2 = Theta1 * a1;
+    a2 = [1; sigmoid(z2)];
+
+    z3 = Theta2 * a2;
+    a3 = sigmoid(z3);
+
+    % 2.
+    y_k = ([1:num_labels] == y(t))';
+
+    delta_3 = a3 - y_k;
+
+    % 3.
+    % adding bias unit for dimensin purpose
+    delta_2 = (Theta2' * delta_3) .* [1; sigmoidGradient(z2)];
+
+    % 4.
+    % removing bias unit
+    delta_2 = delta_2(2:end);
+
+    % 5.
+    Theta1_grad = Theta1_grad + delta_2 * a1';
+    Theta2_grad = Theta2_grad + delta_3 * a2';
+
+end
+
+Theta1_grad = (1/m) * Theta1_grad;
+Theta2_grad = (1/m) * Theta2_grad;
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -62,9 +139,9 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+Theta1_grad = Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
 
-
-
+Theta2_grad = Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
 
 
 
